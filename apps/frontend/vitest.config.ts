4e7a7140b3c2e286cc1bf2ddc60973d44f74bf94
Diogo Sant'Anna <diogosantanna08@gmail.com>
2023-08-09 09:37:42 -0300

    🔧 chore: install and configure vitest

---
+++ apps/frontend/vitest.config.ts
@@ -0,0 +1,24 @@
+/// <reference types="vitest" />
+///<reference types="vite/client" />
+
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    include: ['**/**/*.test.*'],
+    setupFiles: ['jest.setup.js']
+  },
+  resolve: {
+    alias: {
+      '@/components': resolve(__dirname, 'src/app/features/core/components'),
+      '@': resolve(__dirname, 'src/app'),
+    },
+  },
+  root: resolve(__dirname, '.'),
+});
