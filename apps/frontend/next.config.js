0763417cf0cdae7246d51a0ab44b62168f525268
Diogo Sant'Anna <diogosantanna08@gmail.com>
2023-08-13 20:29:51 -0300

    🐛 fix: fix next image component bug

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['127.0.0.1', 'localhost'],
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'http',
+  //       hostname: '127.0.0.1',
+  //       port: '1337',
+  //       pathname: '/**',
+  //     },
+  //     {
+  //       protocol: 'http',
+  //       hostname: 'localhost',
+  //       port: '1337',
+  //       pathname: '/**',
+  //     }
+  //   ]
+  }
+};
 
 module.exports = nextConfig;
